f8c0e2cc743e3c3d5e3116c730fea596d42be03f
jahorinac <registracijae@gmail.com>
2020-05-31 11:12:34 +0200

    08 - firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+
+// Your web app's Firebase  configuration
+var firebaseConfig = {
+
+    // add config data from firebase
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
